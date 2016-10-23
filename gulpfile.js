d55f8cbb587d2d78360a6b43ec5e68ba8a50c82b
cant <cant@MacBook-Pro-Cant.local>
2016-10-23 19:40:40 +0200

    stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass')
+    ,
+    lodash = require('lodash'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+	watchify = require('watchify'),
+	browserify = require('browserify'),
+	gutil = require('gulp-util'),
+	source = require('vinyl-source-stream');
 
 gulp.task('watch', function () {
 
-    gulp.watch('styles/src/**/*.scss', ['sass']);
+    gulp.watch('styles/src/**/*.scss', ['sass'], ['js']);
 
 });
 
@@ -29,4 +35,22 @@ gulp.task('sass', function () {
             .pipe(sourcemaps.write('./maps'))
             .pipe(gulp.dest('styles/dest/'));
 
+});
+
+gulp.task('js', function(){
+	
+	var opts = {
+		entries: ['./scripts/main.js'],
+		debug: true
+	};
+	
+
+	var b =	watchify(browserify(opts));
+			
+	return b.bundle()
+	    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+	    .pipe(source('bundle.js'))
+	    .pipe(sourcemaps.init({loadMaps: true}))
+	    .pipe(sourcemaps.write('./'))
+	    .pipe(gulp.dest('./dist'));
 });
