6b081166abd05225ef7dc4c395ef6ca47152b5d9
Davide Cantelli <cant1989@gmail.com>
2021-05-16 19:24:46 +0200

    chore(ui): logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../images/logo.png";
 
 const Header = (props) => {
   return (
     <header {...props}>
+      <img src={logo} alt="calcolonetto.io logo" width="30" />
       <h1>calcolonetto.io</h1>
     </header>
   );
@@ -13,6 +15,11 @@ export default styled(Header)`
   border-bottom: 1px solid ${(props) => props.theme.colors.border};
   display: flex;
   justify-content: center;
+  align-items: center;
+
+  img {
+    margin-right: 8px;
+  }
 
   h1 {
     margin: 0;
